23982a1660eb5970fbb56eb91a667dd6555633cf
Mark Landeryou <samurai96@hotmail.com>
2023-05-07 19:15:42 -0400

    feat: added the links for footernav component then added it to the footer component

---
+++ src/app/rss/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "RSS",
+  description:
+    "Samples of some work done Mark Landeryou from Sarnia Ontario Canada",
+};
+
+const RSS = () => {
+  return <section className="container mx-auto px-2">RSS</section>;
+};
+
+export default RSS;
---
+++ src/app/stats/page.jsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Statistics",
+  description:
+    "Samples of some work done Mark Landeryou from Sarnia Ontario Canada",
+};
+
+const Stats = () => {
+  return (
+    <section className="container mx-auto px-2">
+      <h2>Statistics</h2>
+    </section>
+  );
+};
+
+export default Stats;
