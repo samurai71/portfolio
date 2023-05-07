737db9f31e0eb6047e4663479611aefc9b62649a
Mark Landeryou <samurai96@hotmail.com>
2023-05-07 18:54:15 -0400

    feat: created the links for topnav component

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Blogs",
+};
+const Blogs = () => {
+  return (
+    <section className="container mx-auto px-2">
+      <h2>Blogs</h2>
+    </section>
+  );
+};
+
+export default Blogs;
---
+++ src/app/utlities/page.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Utilities",
+};
+
+function utlities() {
+  return (
+    <section className="container mx-auto px-2">
+      <h2>Utlities</h2>
+    </section>
+  );
+}
+
+export default utlities;
