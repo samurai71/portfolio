1b02cd5320669b871d8eb15964063a75e655b1b4
Mark Landeryou <samurai96@hotmail.com>
2023-05-23 16:13:39 -0400

    refactor: removed the s from blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,22 @@
 // src/app/blogs/page.tsx
 
-//import { fetchPosts } from "@/lib/devto/fetch";
+import { fetchPosts } from "../../lib/devto/fetch";
 import Link from "next/link";
 
 export const metadata = {
-  title: "Blogs",
+  title: "Blog",
 };
 
 export default async function Page() {
-  //const posts = await fetchPosts();
+  const posts = await fetchPosts();
 
   return (
     <section className="container mx-auto px-2">
-      <h2>Blogs</h2>
+      <h2>Blog</h2>
+      <p>The following posts are from my dev.to blog.</p>
       {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-8">
         {posts.map((post) => (
-          <Link href={`/blogs/${post.slug}`}>{post.title}</Link>
+          <Link href={`/blog/${post.slug}`}>{post.title}</Link>
         ))}
       </div> */}
     </section>
