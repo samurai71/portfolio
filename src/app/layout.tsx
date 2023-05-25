1df4294d7c1e09c42859835b7fe743f45f55caec
Mark Landeryou <samurai96@hotmail.com>
2023-05-25 15:29:39 -0400

    refactor: created a footer for the footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import TopNav from "@/components/TopNav";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
