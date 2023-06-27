260b806a484d25faca2fd4aaa50d3a26d5e37ec9
Mark Landeryou <samurai96@hotmail.com>
2023-06-27 17:26:07 -0400

    style: fixed some styling issues

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -62,6 +62,9 @@ const Contact = () => {
           {/* right */}
           <div className="col-span-3 h-auto w-full rounded-xl shadow-xl shadow-gray-400 lg:p-4">
             <div className="p-4">
+              <div>
+                <p className="text-red-600">* Required Fields</p>
+              </div>
               <form
                 action="https://getform.io/f/dbbd0a26-acb6-4fc1-a87f-b940cd117547"
                 method="POST"
@@ -69,48 +72,61 @@ const Contact = () => {
               >
                 <div className="grid w-full gap-4 py-2 md:grid-cols-2">
                   <div className="flex flex-col">
-                    <label className="py-2 text-sm uppercase">Name</label>
+                    <label className="py-2 text-sm uppercase" for="name">
+                      Name*
+                    </label>
                     <input
                       className="flex rounded-lg border-2 border-gray-300 p-3"
                       type="text"
+                      id="name"
                       name="name"
                       required
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="py-2 text-sm uppercase">
-                      Phone Number
+                    <label className="py-2 text-sm uppercase" for="phone">
+                      Phone Number*
                     </label>
                     <input
                       className="flex rounded-lg border-2 border-gray-300 p-3"
                       type="text"
+                      id="phone"
                       name="phone"
                       required
                     />
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase">Email</label>
+                  <label className="py-2 text-sm uppercase" for="email">
+                    Email*
+                  </label>
                   <input
                     className="flex rounded-lg border-2 border-gray-300 p-3"
                     type="email"
+                    id="email"
                     name="email"
                     required
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase">Subject</label>
+                  <label className="py-2 text-sm uppercase" for="subject">
+                    Subject
+                  </label>
                   <input
                     className="flex rounded-lg border-2 border-gray-300 p-3"
                     type="text"
+                    id="subject"
                     name="subject"
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase">Message</label>
+                  <label className="py-2 text-sm uppercase" for="message">
+                    Message*
+                  </label>
                   <textarea
                     className="rounded-lg border-2 border-gray-300 p-3"
                     rows="10"
+                    id="message"
                     name="message"
                     required
                   ></textarea>
