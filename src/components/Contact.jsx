7cd7386147687013602a5855e90e9bf1fec5e449
Mark Landeryou <samurai96@hotmail.com>
2023-06-27 18:20:28 -0400

    refactor: fixed an error on contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -72,7 +72,7 @@ const Contact = () => {
               >
                 <div className="grid w-full gap-4 py-2 md:grid-cols-2">
                   <div className="flex flex-col">
-                    <label className="py-2 text-sm uppercase" for="name">
+                    <label className="py-2 text-sm uppercase" htmlFor="name">
                       Name*
                     </label>
                     <input
@@ -84,7 +84,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="py-2 text-sm uppercase" for="phone">
+                    <label className="py-2 text-sm uppercase" htmlFor="phone">
                       Phone Number*
                     </label>
                     <input
@@ -97,7 +97,7 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase" for="email">
+                  <label className="py-2 text-sm uppercase" htmlFor="email">
                     Email*
                   </label>
                   <input
@@ -109,7 +109,7 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase" for="subject">
+                  <label className="py-2 text-sm uppercase" htmlFor="subject">
                     Subject
                   </label>
                   <input
@@ -120,7 +120,7 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="py-2 text-sm uppercase" for="message">
+                  <label className="py-2 text-sm uppercase" htmlFor="message">
                     Message*
                   </label>
                   <textarea
