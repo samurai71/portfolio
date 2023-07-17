7058212b8ad8a1820c22142856431a9b91eea8ed
Mark Landeryou <samurai96@hotmail.com>
2023-07-17 07:38:09 -0400

    style: fixed a couple styling issues

--- src/app/(site)/privacy/page.tsx
+++ src/app/(site)/privacy/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+export const metadata = {
+  title: "Privacy Policy",
+  description: "Privacy Policy for Mark Landeryou's personal portfolio",
+};
 
 function privacy() {
-  return <div>privacy policy</div>;
+  return (
+    <section className="container mx-auto px-2">
+      <h2>Privacy Policy</h2>
+    </section>
+  );
 }
 
 export default privacy;
