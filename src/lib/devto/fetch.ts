dffc789bb090b5905506863eec2bde026c5c6e83
Mark Landeryou <samurai96@hotmail.com>
2023-09-04 15:05:15 -0400

    refactor: fixed the variables for env local

--- src/lib/devto/fetch.ts
+++ src/lib/devto/fetch.ts
@@ -4,18 +4,25 @@ import { notFound } from "next/navigation";
 import { Post, PostDetails } from "./types";
 
 export async function fetchPosts(): Promise<Post[]> {
-  const res = await fetch(`https://dev.to/api/articles?username=samurai71`, {
-    next: { revalidate: 3 * 60 * 60 },
-  });
+  const res = await fetch(
+    `https://dev.to/api/articles?username=
+${process.env.DEVTO_USERNAME}`,
+    {
+      next: { revalidate: 3 * 60 * 60 },
+    }
+  );
 
   if (!res.ok) notFound();
   return res.json();
 }
 
 export async function fetchPost(slug: string): Promise<PostDetails> {
-  const res = await fetch(`https://dev.to/api/articles/samurai71/${slug}`, {
-    next: { revalidate: 3 * 60 * 60 },
-  });
+  const res = await fetch(
+    `https://dev.to/api/articles/${process.env.DEVTO_USERNAME}/${slug}`,
+    {
+      next: { revalidate: 3 * 60 * 60 },
+    }
+  );
 
   if (!res.ok) notFound();
   return res.json();
