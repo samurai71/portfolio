c32f43016b87508b7c57557e77ff6353303cc2eb
Mark Landeryou <samurai96@hotmail.com>
2023-09-17 11:48:30 -0400

    refactor: made some edits to the topnav component

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,23 +1,32 @@
 "use client";
 import Link from "next/link";
-//import { FiMoon, FiSun } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 const TopNav = () => {
   return (
-    <nav className="space-x-4 container-md h-8 mt-3 mb-3 pb-2 text-center text-lg">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/utlities">Utilities</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/certificates">Certificates</Link>
-      <Link href="/projects">Projects</Link>
-      <Link href="/contact">Contact</Link>
-      {/* <div className="z-30 transition rounded-full cursor-pointer active:scale-75">
+    <header className="sticky left-0 top-0  h-8 z-10 flex flex-row pb-3 mb-3 container mx-auto border-2">
+      <div className="px-2 basis-1/4">Column 1</div>
+      <nav className="basis-1/2 space-x-2 text-2xl text-indigo-600">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/utlities">Utilities</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/certificates">Certificates</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+        {/* <div className="z-30 transition rounded-full cursor-pointer active:scale-75">
         <FiMoon className="w-6 h-6 transition select-none sm:h-7 sm:w-7 active:scale-75" />
 
         <FiSun className="w-6 h-6 transition select-none sm:h-7 sm:w-7 active:scale-75" />
       </div> */}
-    </nav>
+      </nav>
+      {/* <div className="basis-1/4 px-2">Column 3</div> */}
+      <div className="z-30 transition rounded-full cursor-pointer active:scale-75">
+        <FiMoon className="w-6 h-6 transition select-none sm:h-7 sm:w-7 active:scale-75" />
+
+        {/* <FiSun className="w-6 h-6 transition select-none sm:h-7 sm:w-7 active:scale-75" /> */}
+      </div>
+    </header>
   );
 };
 
