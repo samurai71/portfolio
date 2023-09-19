d66a30012395f00d4b98155877176bb7d6c7bed8
Mark Landeryou <samurai96@hotmail.com>
2023-09-19 16:58:01 -0400

    chore: edited the topnav and updated packages

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -4,8 +4,10 @@ import { FiMoon, FiSun } from "react-icons/fi";
 
 const TopNav = () => {
   return (
-    <header className="sticky left-0 top-0  h-8 z-10 flex flex-row flex-wrap pt-3 pb-3 mb-3 container mx-auto">
-      <div className="px-2 basis-1/4"></div>
+    <header className="sticky left-0 top-0  h-8 z-10 flex flex-row flex-wrap pt-3 pb-3 mb-3 container mx-auto print:hidden">
+      <div className="px-2 basis-1/4">
+        <FiSun className="w-6 h-6 transition select-none sm:h-7 sm:w-7 active:scale-75" />
+      </div>
       <nav className="basis-1/2 space-x-3 text-2xl text-indigo-600">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
