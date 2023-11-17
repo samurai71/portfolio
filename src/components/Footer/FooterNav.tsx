c2afc501c3b07d47c7bf7594a4bd7c720dfd0361
Mark Landeryou <samurai96@hotmail.com>
2023-11-17 17:23:30 -0500

    refactor: removed some commented out code

--- src/components/Footer/FooterNav.tsx
+++ src/components/Footer/FooterNav.tsx
@@ -35,20 +35,6 @@ const FooterNav = () => {
           />
         </div>
       </div>
-      {/* <nav className="space-x-4 container-md h-8 mt-3 mb-3 pb-2 text-center border-2">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/utlities">Utilities</Link>
-        <Link href="/blog">Blog</Link>
-        <Link href="/certificates">Certificates</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/stats">Stats</Link>
-        <Link href="/skills">Skills</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/newsletter">Newsletter</Link>
-        <Link href="/rss">RSS</Link>
-      </nav> */}
     </>
   );
 };
