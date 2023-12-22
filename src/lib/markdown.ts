676be28244827efcdc7886027e42af1f4ea6eafe
Mark Landeryou <samurai96@hotmail.com>
2023-12-22 14:32:46 -0500

    refactor: got dark mode working

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,11 +1,11 @@
 //src/lib/markdown.ts;
 
 import toc from "@jsdevtools/rehype-toc";
-import rehypeHighlight from "rehype-highlight";
+// import rehypeHighlight from "rehype-highlight";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
-import remarkRehype from "remark-rehype";
-import remarkParse from "remark-parse";
+// import remarkRehype from "remark-rehype";
+// import remarkParse from "remark-parse";
 import { unified } from "unified";
 
 export function renderMarkdown(markdown: string): Promise<string> {
